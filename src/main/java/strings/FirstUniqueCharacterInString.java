862368fbcf0a67a5ead846bff4d84af8be120031
sahil-babbar <sahilbabbar12.9@gmail.com>
2025-09-07 17:23:57 +0530

    program to find first unique character in string

---
+++ src/main/java/strings/FirstUniqueCharacterInString.java
@@ -0,0 +1,32 @@
+package main.java.strings;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FirstUniqueCharacterInString {
+    public static void main(String[] args) {
+        String str = new String();
+        Scanner input = new Scanner(System.in);
+        str = input.next();
+
+        HashMap<Character, Integer> charFreqMap = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (charFreqMap.containsKey(str.charAt(i)))
+                charFreqMap.put(str.charAt(i), charFreqMap.get(str.charAt(i)) + 1);
+            else
+                charFreqMap.put(str.charAt(i), 1);
+        }
+
+        boolean uniqueCharFound = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (charFreqMap.get(str.charAt(i)) == 1) {
+                uniqueCharFound = true;
+                System.out.println("first unique character : " + str.charAt(i));
+                break;
+            }
+
+        }
+        if (!uniqueCharFound)
+            System.out.println("no unique char in given string");
+    }
+}
